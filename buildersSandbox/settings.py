fd49e70c76deaa9df23858a01e7b5188b13308bd
Daniel Hong <danielhong35@gmail.com>
2018-04-17 22:40:27 -0700

    Debug is back to True

--- buildersSandbox/settings.py
+++ buildersSandbox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm3k%w9&xf_u99_81fn$q9j1ddl!l%ng&io!g)f7pihez_9m3j$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
