d90a54d4e56b68466a091fdc045db4c6b11131a1
Daniel Hong <danielhong35@gmail.com>
2018-04-19 11:30:34 -0700

    Added Math home page

--- mathapp/urls.py
+++ mathapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.goHome, name='home'),
-    url(r'^subjects/computer_science/index.html', views.computer_science, name='index'),
+    url(r'^subjects/computer_science/cs_index.html', views.computer_science, name='cs_index'),
+    url(r'^subjects/computer_science/math_index.html', views.math, name='math_index'),
 ]
--- mathapp/views.py
+++ mathapp/views.py
@@ -4,4 +4,7 @@ def goHome(request):
 	return render(request, 'mathapp/home.html', {});
 
 def computer_science(request):
-	return render(request, 'mathapp/subjects/computer_science/index.html', {});
+	return render(request, 'mathapp/subjects/computer_science/cs_index.html', {});
+
+def math(request):
+	return render(request, 'mathapp/subjects/math/math_index.html', {});
