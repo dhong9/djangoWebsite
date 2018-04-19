29e63132074e9bfcfcee1efa183f114709536fb4
Daniel Hong <danielhong35@gmail.com>
2018-04-19 09:21:38 -0700

    Added Computer Science home page

--- mathapp/urls.py
+++ mathapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.goHome, name='home'),
+    url(r'^subjects/computer_science/index.html', views.computer_science, name='index'),
 ]
--- mathapp/views.py
+++ mathapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def goHome(request):
 	return render(request, 'mathapp/home.html', {});
+
+def computer_science(request):
+	return render(request, 'mathapp/subjects/computer_science/index.html', {});
