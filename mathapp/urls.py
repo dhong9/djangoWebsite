83ccb2ce73704959c9f5bc9411107af2c0f3e28b
Daniel Hong <danielhong35@gmail.com>
2018-04-21 10:00:24 -0700

    Added math subjects home page

--- mathapp/urls.py
+++ mathapp/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     url(r'^subjects/computer_science/math_index.html', views.math, name='math_index'),
     url(r'^apps/games/games_index.html', views.games, name='games_index'),
     url(r'^apps/tools/tools_index.html', views.tools, name='tools_index'),
+    url(r'^subjects/math/algebrai/algebrai_index.html', views.algebrai, name='algebrai_index'),
+    url(r'^subjects/math/algebraii/algebraii_index.html', views.algebraii, name='algebraii_index'),
+    url(r'^subjects/math/calculusi/calculusi_index.html', views.calculusi, name='calculusi_index'),
+    url(r'^subjects/math/calculusii/calculusii_index.html', views.calculusii, name='calculusii_index'),
+    url(r'^subjects/math/geometry/geometry_index.html', views.geometry, name='geometry_index'),
+    url(r'^subjects/math/algebrai/precalculus_index.html', views.precalculus, name='precalculus_index'),
 ]
--- mathapp/views.py
+++ mathapp/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
 
 def goHome(request):
-	return render(request, 'mathapp/home.html', {});
+	return render(request, 'mathapp/home.html', {})
 
 def computer_science(request):
-	return render(request, 'mathapp/subjects/computer_science/cs_index.html', {});
+	return render(request, 'mathapp/subjects/computer_science/cs_index.html', {})
 
 def math(request):
-	return render(request, 'mathapp/subjects/math/math_index.html', {});
+	return render(request, 'mathapp/subjects/math/math_index.html', {})
 
 def games(request):
-	return render(request, 'mathapp/apps/games/games_index.html', {});
+	return render(request, 'mathapp/apps/games/games_index.html', {})
 
 def tools(request):
-	return render(request, 'mathapp/apps/tools/tools_index.html', {});
+	return render(request, 'mathapp/apps/tools/tools_index.html', {})
+
+def algebrai(request):
+	return render(request, 'mathapp/subjects/math/algebrai/algebrai_index.html', {})
+
+def algebraii(request):
+	return render(request, 'mathapp/subjects/math/algebraii/algebraii_index.html', {})
+
+def calculusi(request):
+	return render(request, 'mathapp/subjects/math/calculusi/calculusi_index.html', {})
+
+def calculusii(request):
+	return render(request, 'mathapp/subjects/math/calculusii/calculusii_index.html', {})
+
+def geometry(request):
+	return render(request, 'mathapp/subjects/math/geometry/geometry_index.html', {})
+
+def precalculus(request):
+	return render(request, 'mathapp/subjects/math/precalculus/precalculus_index.html', {})
